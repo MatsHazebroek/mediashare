c5a2672c6c842afedd6d15330da8f58356bfbce3
mitchellston <m.steenwijk@live.nl>
2023-05-22 11:38:00 +0200

    db studio setup

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,7 +3,20 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({}) => {
-    return "POEP ETEN :)";
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: {
+        id: true,
+        User: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
   }),
 });
